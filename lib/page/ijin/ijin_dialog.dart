77c7a29551a9d4f31134ffd45928a332cfe0de76
Ilham Dohaan <doha39anilham@gmail.com>
2024-12-04 17:26:14 +0700

    z

--- lib/page/ijin/ijin_dialog.dart
+++ lib/page/ijin/ijin_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:eportal/assets/color/custom_color.dart';
 import 'package:eportal/style/custom_date_picker.dart';
 import 'package:eportal/style/custom_font.dart';
 import 'package:eportal/util/converter.dart';
@@ -7,6 +8,7 @@ import 'package:eportal/util/toast.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+
 class IjinDialog{
   static void showIjinDialog(BuildContext ctx){
 
@@ -14,8 +16,8 @@ class IjinDialog{
 
     TimeOfDay startTime = TimeOfDay.now();
     TimeOfDay endTime = TimeOfDay.now();
-
     DateTime startDate = DateTime.now();
+    bool isDoctorLetterAvailable = false;
 
     List<String> tipeIjin = [
       'Ijin Terlambat',
@@ -28,12 +30,153 @@ class IjinDialog{
       'Ijin Lainnnya',
     ];
 
+
     TextEditingController tfReason = TextEditingController();
+    
+
     showDialog(
       context: ctx,
       builder: (BuildContext ctx){
         return StatefulBuilder(
           builder: (BuildContext ctxStateful, StateSetter setState) {
+
+                      
+            Widget radioLetterWidget() {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Surat Dokter',
+                    style: CustomFont.headingEmpatSemiBold(),
+                  ),
+                  const SizedBox(
+                    height: 2,
+                  ),
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.grey)),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                            flex: 1,
+                            child: Row(
+                              children: [
+                                Radio(
+                                    value: true,
+                                    activeColor: CustomColor.primary(),
+                                    groupValue: isDoctorLetterAvailable,
+                                    onChanged: (state) {
+                                      setState(() {
+                                        isDoctorLetterAvailable = state!;
+                                      });
+                                    }),
+                                AutoSizeText(
+                                  'Ada',
+                                  style: CustomFont.headingEmpat(),
+                                )
+                              ],
+                            )),
+                        Expanded(
+                            flex: 1,
+                            child: Row(
+                              children: [
+                                Radio(
+                                    value: false,
+                                    activeColor: CustomColor.primary(),
+                                    groupValue: isDoctorLetterAvailable,
+                                    onChanged: (state) {
+                                      setState(() {
+                                        isDoctorLetterAvailable = state!;
+                                      });
+                                    }),
+                                AutoSizeText(
+                                  'Tidak Ada',
+                                  style: CustomFont.headingEmpat(),
+                                )
+                              ],
+                            )),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            }
+
+            Widget startTimeWidget() {
+              return Column(
+                children: [
+                  AutoSizeText(
+                    'Jam',
+                    style: CustomFont.headingEmpatSemiBold(),
+                    maxLines: 1,
+                  ),
+                  const SizedBox(
+                    height: 2,
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      final selectedTime = await showTimePicker(
+                        initialTime: TimeOfDay.now(),
+                        context: ctx,
+                      );
+                      if (selectedTime != null) {
+                        startTime = selectedTime;
+                      }
+                    },
+                    child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 6, horizontal: 12),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1, color: Colors.grey)),
+                        child: Text(
+                          '${startTime.hour}:${startTime.minute}',
+                          style: CustomFont.headingEmpat(),
+                        )),
+                  ),
+                ],
+              );
+            }
+
+            Widget endTimeWidget() {
+              return Column(
+                children: [
+                  AutoSizeText(
+                    'Jam',
+                    style: CustomFont.headingEmpatSemiBold(),
+                    maxLines: 1,
+                  ),
+                  const SizedBox(
+                    height: 2,
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      final selectedTime = await showTimePicker(
+                        initialTime: TimeOfDay.now(),
+                        context: ctx,
+                      );
+                      if (selectedTime != null) {
+                        endTime = selectedTime;
+                      }
+                    },
+                    child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 6, horizontal: 12),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1, color: Colors.grey)),
+                        child: Text(
+                          '${endTime.hour}:${endTime.minute}',
+                          style: CustomFont.headingEmpat(),
+                        )),
+                  ),
+                ],
+              );
+            }
             return AlertDialog(
               iconPadding: const EdgeInsets.all(0),
               insetPadding: const EdgeInsets.all(0),
@@ -142,33 +285,6 @@ class IjinDialog{
                             ),
                             contentPadding: const EdgeInsets.symmetric(
                                 vertical: 8, horizontal: 12))
-                    ),
-                    AutoSizeText(
-                      'Jam',
-                      style: CustomFont.headingEmpatSemiBold(),
-                      maxLines: 1,
-                    ),
-                    const SizedBox(
-                      height: 2,
-                    ),
-                    InkWell(
-                      onTap: () async {
-                        final selectedTime = await showTimePicker(
-                          initialTime: TimeOfDay.now(),
-                          context: ctx,
-                        );
-
-                        if (selectedTime != null) {
-                          startTime = selectedTime;
-                        }
-                      },
-                      child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                              border: Border.all(width: 1, color: Colors.grey)),
-                          child: Text('${startTime.hour}:${startTime.minute}' ,style: CustomFont.headingEmpat(),
-                          )),
                     ),
                   ],
                 ),
